a0154ee16475d99f9edb3acd71bc69a166649753
cynthiachiu <cynthia.ms.chiu@gmail.com>
2025-04-25 10:58:09 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const exoplanetRoutes = require('./routes/exoplanetRoutes');
+const { initializeData } = require('./services/exoplanetService');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,6 @@ app.use('/api', exoplanetRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
-  const { initializeData } = require('./services/exoplanetService');
+  
   await initializeData();
 });
